b52eb9a96a9a99e32d9e471560bc905930546744
jaime <jaime.molina@mattersupply.co>
2020-08-29 16:47:11 -0500

    Users Seed

--- backend/mix.exs
+++ backend/mix.exs
@@ -43,7 +43,7 @@ defmodule Backend.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
       {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"}
     ]
   end
 
--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -1,11 +1,34 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Backend.Repo.insert!(%Backend.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Backend.Repo
+alias Backend.Vacation.{Booking, Place, Review}
+alias Backend.Accounts.User
+
+"""
+@doc Users creation
+"""
+
+mike =
+  %User{}
+  |> User.changeset(%{
+    username: "mike",
+    email: "mike@example.com",
+    password: "secret"
+  })
+  |> Repo.insert!()
+
+nicole =
+  %User{}
+  |> User.changeset(%{
+    username: "nicole",
+    email: "nicole@example.com",
+    password: "secret"
+  })
+  |> Repo.insert!()
+
+beachbum =
+  %User{}
+  |> User.changeset(%{
+    username: "brachbum",
+    email: "beachbum@example.com",
+    password: "secret"
+  })
+  |> Repo.insert!()
